9ce5fba35db35c764ead58f778f7513707390c38
Carlos Andres Rodriguez <rwkama27@gmail.com>
2020-11-13 02:16:32 -0300

    first

---
+++ XamGps/XamGps/VerOrdenEnvio.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamGps
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class VerOrdenEnvio : ContentPage
+	{
+		public VerOrdenEnvio (OrdenEnvio orden)
+		{
+			InitializeComponent ();
+            var signalr = new Signalr();
+          
+            signalr.EnviarCoordenadas(orden.IdOE);
+            BindingContext = orden;
+            btnDestinoD.Clicked += BtnDestinoM_Clicked;
+            btnEnvioD.Clicked += BtnEnvioD_Clicked;
+
+        }
+        
+        private async void BtnDestinoM_Clicked(object sender, EventArgs e)
+        {
+            var ordenenviosesion = Application.Current.Properties["OrdenEnvio"];
+            var orden = (OrdenEnvio)ordenenviosesion;
+            var placemark = new Placemark
+                {
+                    CountryName = "Uruguay",
+                    Locality = orden.LocalidadOE,
+                    Thoroughfare = orden.UbicacionOE,
+                };
+                var options = new MapLaunchOptions { Name = orden.UbicacionOE };
+
+                await Xamarin.Essentials.Map.OpenAsync(placemark);
+        }
+        private  void BtnEnvioD_Clicked(object sender, EventArgs e)
+        {
+
+            var ordenenvios = Application.Current.Properties["OrdenEnvio"];
+            var o = (OrdenEnvio)ordenenvios;
+            var signalr = new Signalr();
+             signalr.EnviarCoordenadas(o.IdOE);
+        }
+    }
+}
---
+++ XamGps/XamGps/VerOrdenEnvioCliente.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Plugin.Geolocator.Abstractions;
+using System;
+using Xamarin.Forms;
+
+using Xamarin.Forms.Xaml;
+
+namespace XamGps
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class VerOrdenEnvioCliente : ContentPage
+	{
+        Position addressPosition;
+        HubConnection hubConnection;
+       
+        public VerOrdenEnvioCliente ()
+		{
+            InitializeComponent();
+            RecibirLocation();
+            btnRecibir.Clicked += BtnVerC_Clicked;
+
+        }
+        private  void BtnVerC_Clicked(object sender, EventArgs e)
+        {
+             RecibirLocation();
+
+        }
+        private  void RecibirLocation()
+        {
+            hubConnection = new HubConnectionBuilder()
+               .WithUrl($"http://www.signalrcarlos.somee.com/transport")
+               .Build();
+             hubConnection.StartAsync();
+            hubConnection.On<int,double, double>("locationMessage", (orden,latitude, longitude) =>
+            {
+                addressPosition = new Position(latitude, longitude);
+
+                 Xamarin.Essentials.Map.OpenAsync(addressPosition.Latitude, addressPosition.Longitude);
+            });
+         
+        }
+       
+
+    }
+}
